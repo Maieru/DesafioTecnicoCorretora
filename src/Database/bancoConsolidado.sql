08d7b936e9ffe0c28d95dba4d7f573e3a71af0bc
Maieru <soljoaomaieru@hotmail.com>
2025-06-04 19:25:43 -0300

    Pequena correção no tipo de data da tabela de cotações

--- src/Database/bancoConsolidado.sql
+++ src/Database/bancoConsolidado.sql
@@ -26,7 +26,7 @@ CREATE TABLE [dbo].[tbCotacoes] (
     [id]             INT IDENTITY(1,1) PRIMARY KEY,
     [ativo_id]       INT               NOT NULL REFERENCES tbAtivos(id),
     [preco_unitario] DECIMAL(10,2)     NOT NULL,
-    [data_hora]      DATETIME          NOT NULL
+    [data_hora]      DATETIME2          NOT NULL
 );
 GO
 
